64bbed54dd188552f65e6af40fc5d0ace0306354
kalyanisangi <65577201+kalyanisangi@users.noreply.github.com>
2020-05-19 00:43:11 -0400

    carto
    carton

---
+++ CandidateProject/ViewModels/AddEquipmentViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CandidateProject.ViewModels
+{
+    public class AddEquipmentViewModel
+    {
+        public int CartonId { get; set; }
+        public int EquipmentId { get; set; }
+
+        public string  Error { get; set; }
+    }
+}
